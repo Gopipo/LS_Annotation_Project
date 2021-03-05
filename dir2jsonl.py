6f19d7e7f4bfb4c645276b7f6b1e4766ad7807d8
Gopipo <50098441+Gopipo@users.noreply.github.com>
2021-03-05 16:14:19 +0100

    updated to include text directly & fixed paths

--- dir2jsonl.py
+++ dir2jsonl.py
@@ -8,26 +8,31 @@ import json
 
 imageDirectory = "./Corpora/images_webcorpus/"
 textDirectory = "./Corpora/tokenized_txt_files/tokenized_"
-# text directory selector (m: monolingual, p: parallel)
 mode = ""
 
-def getMode(path):
+def getMode(path) -> str:
     """ Pre: file structure is thus that directory containing parallel or
-    monolingual text files is marked as such. "parallel" or "monolingual"
-    must be the end of the directory name and separated by "_".
-    
+    monolingual text files is marked as such. "_parallel" or "_monolingual"
+    must be the end of the directory name.
+
     Checks whether loop currently is in a parallel or monolingual directory
     and sets mode to the corresponding option..
     """
-    currentMode = re.findall("_([A-Za-z0-9]+)$", path) 
+    currentMode = re.findall("_([A-Za-z0-9]+)$", path)
     return currentMode[0]
-    
+
 def writeJSON(imagePath, textPath):
-    """ Appends JSON Lines file with image and text path pair to be fed to prodigy 
+    """ Appends JSON Lines file with image path and text to be fed to prodigy
     as stream for annotation.
     """
-    #JSON format
-    dict = {"image": imagePath, "text": textPath}
+    #read txt file content
+    content=""
+    with open(textPath, "r", encoding="utf-8") as tf:
+        for line in tf:
+            content = content + line
+
+    #create JSON format
+    dict = {"image": imagePath, "text": content}
     #append file
     with open("./Corpora/JSON/imageTextPaired.jsonl", "a", encoding="utf-8") as f:
         json.dump(dict, f)
@@ -36,9 +41,9 @@ def writeJSON(imagePath, textPath):
 def main():
     """Pre: Only execute this once. Appends imageTextPaired.jsonl file!
     Exception: completely new set of image data in the given directories.
-    
+
     Crawls through all image directories, pairs up the image path with the
-    corresponding text file path, appends the pair to the specified .jsonl
+    corresponding text, appends the pair to the specified .jsonl
     file..
     """
     # alert user if .jsonl file already exists
@@ -48,24 +53,24 @@ def main():
             appendVerification = input("The file ./Corpora/JSON/imageTextPaired.jsonl already exists. \n Are you sure you want to append said file with the data in: ./Corpora/images_webcorpus/ \n (y/n): ")
         if appendVerification == "n":
             sys.exit("Execution stopped.")
-            
+
     # images_html/, images_pdf/
     for subdirectory0 in os.listdir(imageDirectory):
         subdirectory0 += "/"
         # */*_monolingual/, */*_parallel/
-        for subdirectory1 in os.listdir(imageDirectory 
+        for subdirectory1 in os.listdir(imageDirectory
                                        + subdirectory0):
-        
+
             mode = getMode(subdirectory1)
 
             subdirectory1 += "/"
-            
+
             currentTextDirectory = textDirectory + mode + "/"
             # file level
-            for filename in os.listdir(imageDirectory 
-                                       + subdirectory0 
+            for filename in os.listdir(imageDirectory
+                                       + subdirectory0
                                        + subdirectory1):
-                imagePath = subdirectory1 + filename
+                imagePath = imageDirectory + subdirectory0 + subdirectory1 + filename
                 #extract corpus file number from image file
                 if mode == "monolingual":
                     corpusFileNr = re.findall("^([0-9]+)_", filename)
@@ -73,8 +78,8 @@ def main():
                     corpusFileNr =re.findall("^([0-9]+_[A-Z]{2})_", filename)
                 else:
                     sys.exit("Fatal error at " + imagePath + " Could not resolve corpus file number.")
-                
-                textPath = textDirectory + mode + "/" + corpusFileNr[0] + ".txt"
+
+                textPath = currentTextDirectory + corpusFileNr[0] + ".txt"
                 writeJSON(imagePath, textPath)
 
 if __name__ == "__main__":
